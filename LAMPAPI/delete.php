1c2af1baed9cfed550c0e91329d8c1a7f29d2b29
Jason Lin <jasonlinfl@gmail.com>
2022-09-03 14:57:21 -0400

    Revert "Update delete.php"
    This reverts commit aba3cb7cd96dd08237dc8087f1f26d7bb6144412.

--- LAMPAPI/delete.php
+++ LAMPAPI/delete.php
@@ -16,14 +16,13 @@ $inData = getRequestInfo();
         $stmt->execute();
         $result = $stmt->get_result();
 
-		//if it is in the database delete
         if(mysqli_num_rows($result) > 0){
 
             $stmt = $conn->prepare("DELETE from Contacts where Email like ? and ID=?");
             $stmt->bind_param("si", $email, $inData["ID"]);
             $stmt->execute();
 
-        }else{//return error
+        }else{
             returnWithError("Contact not found");
         }
 		

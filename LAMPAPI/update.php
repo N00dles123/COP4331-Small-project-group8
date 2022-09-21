d45618f6d1a110d12d837d6f4b7c11f2020bed58
DaniOstos <101365912+DaniOstos@users.noreply.github.com>
2022-09-21 09:34:08 -0400

    Changed contactID with UserID

--- LAMPAPI/update.php
+++ LAMPAPI/update.php
@@ -14,10 +14,10 @@ $inData = getRequestInfo();
         $lastName = $inData["LastName"];
         $email = $inData["Email"];
         $phone = $inData["Phone"];
-        $contactID = $inData["ID"];
+        $userID = $inData["UserID"];
 
-        $stmt = $conn->prepare("UPDATE Contacts SET FirstName =?, LastName =?, Email =?, Phone =? WHERE ID=?");
-        $stmt->bind_param("ssssi", $firstName, $lastName, $email, $phone, $contactID);
+        $stmt = $conn->prepare("UPDATE Contacts SET FirstName =?, LastName =?, Email =?, Phone =? WHERE UserID=?");
+        $stmt->bind_param("ssssi", $firstName, $lastName, $email, $phone, $userID);
         $stmt->execute();
         
 		$stmt->close();
